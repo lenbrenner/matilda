d18561f195f95e3047a197509e5858c6dc893ef9
Leonhard Brenner <len.brenner@takeoff.com>
2021-10-31 09:27:32 -0400

    Adding gRPC to the mix.

--- README.md
+++ README.md
@@ -1,8 +1,35 @@
-Matilda is a waltzer.
+# Description
+The route guide server and client demonstrate how to use grpc go libraries to
+perform unary, client streaming, server streaming and full duplex RPCs.
 
-The purpose of this project is to explore paths through floor. I am using
-Location to represent a square on a grid. The square have transitions from
-which we can build a graph. On initialization, we will build all paths from
-one location to another. By calculating these ahead of time we can have
-path tables which should make decision-making more efficient and hopefully
-expose some other useful tricks such as collision prediction.
+Please refer to [gRPC Basics: Go](https://grpc.io/docs/tutorials/basic/go.html) for more information.
+
+See the definition of the route guide service in routeguide/route_guide.proto.
+
+# Run the sample code
+To compile and run the server, assuming you are in the root of the route_guide
+folder, i.e., .../examples/route_guide/, simply:
+
+```sh
+$ go run server/server.go
+```
+
+Likewise, to run the client:
+
+```sh
+$ go run client/client.go
+```
+
+# Optional command line flags
+The server and client both take optional command line flags. For example, the
+client and server run without TLS by default. To enable TLS:
+
+```sh
+$ go run server/server.go -tls=true
+```
+
+and
+
+```sh
+$ go run client/client.go -tls=true
+```
---
+++ client/client.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/examples/data"
+	pb "takeoff.com/matilda/api"
+)
+
+var (
+	tls                = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	caFile             = flag.String("ca_file", "", "The file containing the CA root cert file")
+	serverAddr         = flag.String("server_addr", "localhost:10000", "The server address in the format of host:port")
+	serverHostOverride = flag.String("server_host_override", "x.test.example.com", "The server name used to verify the hostname returned by the TLS handshake")
+)
+
+// printFeature gets the feature for the given point.
+func printFeature(client pb.MatildaClient, point *pb.Point) {
+	log.Printf("Getting feature for point (%d, %d)", point.Latitude, point.Longitude)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	feature, err := client.GetSquare(ctx, point)
+	if err != nil {
+		log.Fatalf("%v.GetFeatures(_) = _, %v: ", client, err)
+	}
+	log.Println(feature)
+}
+
+func main() {
+	flag.Parse()
+	var opts []grpc.DialOption
+	if *tls {
+		if *caFile == "" {
+			*caFile = data.Path("x509/ca_cert.pem")
+		}
+		creds, err := credentials.NewClientTLSFromFile(*caFile, *serverHostOverride)
+		if err != nil {
+			log.Fatalf("Failed to create TLS credentials %v", err)
+		}
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	} else {
+		opts = append(opts, grpc.WithInsecure())
+	}
+
+	opts = append(opts, grpc.WithBlock())
+	conn, err := grpc.Dial(*serverAddr, opts...)
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewMatildaClient(conn)
+
+	// Looking for a valid feature
+	printFeature(client, &pb.Point{Latitude: 409146138, Longitude: -746188906})
+
+	// Feature missing.
+	printFeature(client, &pb.Point{Latitude: 0, Longitude: 0})
+}
