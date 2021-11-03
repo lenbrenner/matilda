bfff40bfb9a28daf7429c1289c0ce7d43172daa2
Leonhard Brenner <len.brenner@takeoff.com>
2021-11-03 11:02:29 -0400

    Delete turd.

--- main.go
+++ main.go
@@ -4,17 +4,6 @@ import (
 	"takeoff.com/matilda/applications"
 )
 
-//Todo this needs implementing.
-//func pathsTo(location model.Location, destination model.Location) {
-//	//fmt.Printf("Searching for paths from %v to %v")
-//	//seen := map[LocationLabel]bool{}
-//	for direction, label := range location.Transitions {
-//		//node := locations[label]
-//		fmt.Println("%v, %v", direction, label)
-//
-//	}
-//}
-
 func main() {
 	var app = applications.InitApplication()
 	app.LocationService.Display()
